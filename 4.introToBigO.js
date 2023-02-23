199e4c5ae32f15a7074f3f51cadafcb430001332
Andrei B <andreibarut@gmail.com>
2023-02-23 18:55:43 +0200

    first commit

---
+++ 4.introToBigO.js
@@ -0,0 +1,13 @@
+/*
+
+Big O Notation
+
+*/
+
+//dummy task : create a function that returns a reversed string
+
+const reverseString = (str) => {
+	return Array.from(str).reverse().join("");
+};
+
+console.log(reverseString("andrei")); //correct
